2fd38b9075281dcd26938747f9deae769f210d14
UltimateQuack <flamejantepato77@gmail.com>
2024-10-27 15:00:01 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+// play gooyfy bg music
+music.play(music.stringPlayable("C - D - E - F G ", 180), music.PlaybackMode.LoopingInBackground)
+basic.showString("Hello!")
+basic.pause(100)
+basic.showString("I can talk!")
+basic.pause(500)
+basic.showString("Or maybe not?")
+basic.pause(100)
+basic.clearScreen()
